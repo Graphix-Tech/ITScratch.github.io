1a3917adbcca436a6d516ce4bc2877c4a50187fc
puja0709 <102405052+puja0709@users.noreply.github.com>
2023-09-01 12:43:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
+function initializeScratchCard(selectedImageIndex) {
+    $("#card").wScratchPad({
+        size: 100, // The size of the brush/scratch.
+        bg: `Images/Gpay_Card ${selectedImageIndex}.jpg`, // Background image path.
+        fg: `Images/front.jpg`, // Foreground image path.
+        cursor: "pointer", // Set cursor.
+        scratchUp: function () {
+            // Add any action you want to perform when the scratch card is revealed
+            alert("Scratch card revealed!");
+        },
+    });
+}
+
+// Check if the scratch card has been revealed before and retrieve the selected image index
 let isRevealed = localStorage.getItem("isRevealed") === "true";
-        let selectedImageIndex = localStorage.getItem("selectedImageIndex");
+let selectedImageIndex = localStorage.getItem("selectedImageIndex");
 
-        $(document).ready(function () {
-            if (!isRevealed) {
-                // The following code initializes the scratch card with the shuffling images and reveal logic
-                const numImages = 4; // Number of available images
-                if (!selectedImageIndex) {
-                    selectedImageIndex = Math.floor(Math.random() * numImages) + 1;
-                    localStorage.setItem("selectedImageIndex", selectedImageIndex);
-                }
-
-                $("#card").wScratchPad({
-                    size: 100, // The size of the brush/scratch.
-                    bg: `Images/Gpay_Card ${selectedImageIndex}.jpg`, // Background image path.
-                    fg: `Images/front.jpg`, // Foreground image path.
-                    cursor: "pointer", // Set cursor.
-                    scratchUp: function () {
-                        if (!isRevealed) {
-                            isRevealed = true;
-                            // Save the revealed state in local storage
-                            localStorage.setItem("isRevealed", "true");
-                            // Add any action you want to perform when the scratch card is revealed
-                            alert("Scratch card revealed!");
-                        }
-                    },
-                });
-            } else {
-                // If the card has been revealed, you can show a message or perform other actions
-                alert("Scratch card already revealed!");
-            }
-        });
+// Initialize or restore the scratch card based on whether it has been revealed before
+$(document).ready(function () {
+    if (!isRevealed) {
+        // If the card has not been revealed, generate a random image index
+        const numImages = 4; // Number of available images
+        selectedImageIndex = Math.floor(Math.random() * numImages) + 1;
+        initializeScratchCard(selectedImageIndex);
+        localStorage.setItem("selectedImageIndex", selectedImageIndex);
+    } else {
+        // If the card has been revealed, load the last scratched card
+        if (selectedImageIndex) {
+            initializeScratchCard(selectedImageIndex);
+        }
+    }
+});
